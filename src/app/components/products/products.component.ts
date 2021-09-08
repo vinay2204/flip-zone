c9ffcf463f2bad311ac6ae0b22114779bde4661f
Vinay kumar <vinay.k.rohilla@gmail.com>
2021-09-08 21:14:15 +0530

    cart01

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -8,11 +9,22 @@ import { ApiService } from 'src/app/service/api.service';
 })
 export class ProductsComponent implements OnInit {
   public productList: any;
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService,
+    private cartService: CartService) {}
 
   ngOnInit(): void {
     this.apiService.getProduct().subscribe((res) => {
       this.productList = res;
+
+      this.productList.forEach((a:any) => {
+        Object.assign(a,{quantity:1,total: a.price});
+      });
     });
+  }
+
+  addtocart(item:any){
+    this.cartService.addtoCart(item);
+   
+    
   }
 }
